22f769d144459703c75365aa978df808dbc6b933
John Ellis <67706810+jaxellis@users.noreply.github.com>
2021-04-10 11:49:58 -0700

    Initial Upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# powercord-nsfw-badge
+# Better NSFW Badge - Powercord - [![Paypal][paypal-badge]][paypal-link]
+
+[paypal-badge]: https://img.shields.io/badge/Paypal-Donate!-%23003087.svg?logo=paypal&style=flat
+[paypal-link]: https://paypal.me/JaxEllis
+
+Based on mwittrien's BetterNSFWTag for BetterDiscord. This provides the same functionality for Powercord users by adding a noticeable badge to NSFW channels.
+
+![Screenshot](https://i.imgur.com/CYGfBmq.png)
---
+++ index.js
@@ -0,0 +1,33 @@
+const { Plugin } = require('powercord/entities');
+const { getModule, React } = require('powercord/webpack');
+const { inject, uninject } = require('powercord/injector');
+
+module.exports = class NSFWBadge extends Plugin {
+	async startPlugin() {
+		const iconClasses = await getModule(['iconItem']);
+		const badgeClasses = await getModule(['textBadge']);
+		const channelItem = await getModule((m) => m.default && m.default.displayName == 'ChannelItem');
+
+		inject('nsfw-badge', channelItem, 'default', (args) => {
+			if (args[0]['channel']['nsfw'] == false) {return args;}
+			args[0].children.push(
+				React.createElement('div', {
+					className: 'nsfw-badge ' + iconClasses.iconBase,
+					children: React.createElement('div', {
+						className:
+							badgeClasses.textBadge + ' ' + badgeClasses.baseShapeRound,
+						style: {
+							borderRadius: '3px',
+							backgroundColor: 'rgb(240, 71, 71)',
+						},
+					}, 'NSFW'),
+				})
+			);
+			return args;
+		}, true);
+	}
+
+	pluginWillUnload() {
+		uninject('nsfw-badge');
+	}
+};
